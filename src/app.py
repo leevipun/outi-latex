54773298b7ac5459791cf62e37cad67ea058a2f0
leevipun <leevi.puntanen2@gmail.com>
2025-11-10 21:47:03 +0200

    Refactor app routes to remove todo functionality and integrate reference retrieval

--- src/app.py
+++ src/app.py
@@ -1,35 +1,12 @@
-from flask import redirect, render_template, request, jsonify, flash
+from flask import jsonify, render_template
 from db_helper import reset_db
-from repositories.todo_repository import get_todos, create_todo, set_done
 from config import app, test_env
-from util import validate_todo
+from utils import references
 
 @app.route("/")
 def index():
-    todos = get_todos()
-    unfinished = len([todo for todo in todos if not todo.done])
-    return render_template("index.html", todos=todos, unfinished=unfinished) 
-
-@app.route("/new_todo")
-def new():
-    return render_template("new_todo.html")
-
-@app.route("/create_todo", methods=["POST"])
-def todo_creation():
-    content = request.form.get("content")
-
-    try:
-        validate_todo(content)
-        create_todo(content)
-        return redirect("/")
-    except Exception as error:
-        flash(str(error))
-        return  redirect("/new_todo")
-
-@app.route("/toggle_todo/<todo_id>", methods=["POST"])
-def toggle_todo(todo_id):
-    set_done(todo_id)
-    return redirect("/")
+    reference_types = references.get_all_references()
+    return render_template("index.html", reference_types=reference_types)
 
 # testausta varten oleva reitti
 if test_env:
