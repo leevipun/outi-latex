8eb0fbcbc5e68ed578bcc8b0c72158684a69fe46
leevipun <leevi.puntanen2@gmail.com>
2025-11-13 15:38:02 +0200

    change to src. import

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -16,8 +16,8 @@ import pytest
 def app():
     """Create and configure a Flask app for testing."""
     # Import app to register all routes
-    import app as app_module  # noqa: F401
-    from config import app as flask_app
+    from src import app as app_module  # noqa: F401
+    from src.config import app as flask_app
 
     # Set testing mode
     flask_app.config["TESTING"] = True
