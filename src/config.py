e4429b14cc3e71ce399312181d45c2d7292cbc05
leevipun <leevi.puntanen2@gmail.com>
2025-11-13 16:22:24 +0200

    Add Flask application configuration and database setup

---
+++ src/config.py
@@ -0,0 +1,16 @@
+"""Flask application configuration and database setup."""
+from os import getenv
+
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
+test_env = getenv("TEST_ENV") == "true"
+print(f"Test environment: {test_env}")
+
+app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
