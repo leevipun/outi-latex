42b19fb6d8601a33761e1c0d06a88b023ef87d39
vainiovesa <vesa.vainio@helsinki.fi>
2025-11-25 16:54:11 +0200

    Add function for adding tags

--- src/utils/tags.py
+++ src/utils/tags.py
@@ -11,8 +11,25 @@ class TagError(Exception):
     pass
 
 
-def add_tags():
-    ...
+class TagExistsError(Exception):
+    """Raised when trying to add existing tag."""
+
+    pass
+
+
+def add_tag(tag:str):
+    sql = text(
+        """ INSERT INTO tags (name)
+            VALUES (:tag);
+        """
+    )
+    try:
+        db.session.execute(sql, {"tag": tag})
+        db.session.commit()
+
+    except Exception as e:
+        db.session.rollback()
+        raise TagExistsError(f"Failed to add tag {tag}: {e}.")
 
 
 def get_tags():
