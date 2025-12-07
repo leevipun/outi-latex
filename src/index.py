5597e908cdf36418095f8f8f34351e4526267a87
leevipun <leevi.puntanen2@gmail.com>
2025-12-07 19:46:10 +0200

    Add deployment step to CI workflow and update Flask app port configuration

--- src/index.py
+++ src/index.py
@@ -3,8 +3,10 @@
 This module serves as the entry point for the Flask application.
 It imports the Flask application instance and runs it on a specified host and port.
 """
-
+import os
 from src.app import app
 
+port = int(os.getenv("PORT", 5001))
+
 if __name__ == "__main__":
-    app.run(port=5001, host="0.0.0.0", debug=True)
+    app.run(port=port, host="0.0.0.0", debug=True)
